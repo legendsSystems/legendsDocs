b463a09746434434f0949cdeeff037577a752f56
Witt <adam.witt@iaawg.com>
2024-07-05 18:05:19 -0500

    basic branding

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>legendsDocs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/legendsSystems/legendsDocs",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com/legendsSystems",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/legendsSystems/legendsDocs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "legendsSystems",
   },
-}
+  gitTimestamp: "Last updated on",
+};
 
-export default config
+export default config;
